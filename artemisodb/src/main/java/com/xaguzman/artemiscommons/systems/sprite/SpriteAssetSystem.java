438677b63ef98888348a580202dab9c8b6bb368c
Xavier Guzman <xavier.guzman@flex.com>
2019-04-03 16:09:54 -0600

    updated SpriteAssetSystem to expose more information to interested parties

--- artemisodb/src/main/java/com/xaguzman/artemiscommons/systems/sprite/SpriteAssetSystem.java
+++ artemisodb/src/main/java/com/xaguzman/artemiscommons/systems/sprite/SpriteAssetSystem.java
@@ -57,10 +57,34 @@ public class SpriteAssetSystem extends PassiveSystem {
     }
 
     /**
+     * Allows to create a duplicate of spriteName and register it under the "alias" key.
+     * Useful to store transformed sprites
+     * @param spriteName
+     * @param alias
+     */
+    public Sprite createSprite(String spriteName, String alias){
+        Sprite spr= defaultAtlas.createSprite(spriteName);
+        sprites.put(alias, spr);
+        return spr;
+    }
+
+    /**
+     * Allows to create a duplicate of spriteName and register it under the "alias" key.
+     * Useful to store transformed sprites
+     * @param spriteName
+     * @param alias
+     */
+    public Sprite createSprite(String spriteName, String alias, String atlasId){
+        Sprite spr= atlasses.get(atlasId).createSprite(spriteName);
+        sprites.put(alias, spr);
+        return spr;
+    }
+
+    /**
      * will read all the static sprites comming from the given texture atlas.
      *
      * The provided texture atlas will be disposed once this system is disposed
-     * TO READ ANIMATIONS YOU NEED TO CALL {@link #loadAnimation(String, String, float)}
+     * TO READ ANIMATIONS YOU NEED TO CALL {@link #createAnimation(String, float)}
      */
     public void addAtlas(TextureAtlas atlas, String id){
         addAtlas(atlas, id, true);
@@ -71,7 +95,7 @@ public class SpriteAssetSystem extends PassiveSystem {
      * Reads all the static sprites comming from the given texture atlas.
      *</p>
      * This system will store and dispose the texture atlas if ownAtlas is true
-     * TO READ ANIMATIONS YOU NEED TO CALL {@link #loadAnimation(String, String, float)}
+     * TO READ ANIMATIONS YOU NEED TO CALL {@link #createAnimation(String, String, float)}
      */
     public void addAtlas(TextureAtlas atlas, String id, boolean ownAtlas){
         ownsAtlasses.put(id, ownAtlas);
@@ -95,9 +119,9 @@ public class SpriteAssetSystem extends PassiveSystem {
     }
 
     /**
-     *  Loads an animation from the default atlas
+     *  Loads an animation from the default atlas. The sprites are retrieved from the atlas and the animation will keep the id as the alias
      * */
-    public Animation<Sprite> loadAnimation(String id, float frameDuration){
+    public Animation<Sprite> createAnimation(String id, float frameDuration){
         Array<Sprite> sprites = defaultAtlas.createSprites(id);
 
         Animation<Sprite> animation = new Animation<Sprite>(frameDuration, sprites);
@@ -107,13 +131,37 @@ public class SpriteAssetSystem extends PassiveSystem {
     }
 
     /**
-     *  Loads an animation from a previously registered atlas
+     *  Loads an animation from a previously registered atlas. The sprites are retrieved from the atlas and the animation will keep the id as the alias / key
      * */
-    public Animation<Sprite> loadAnimation(String id, String atlasId, float frameDuration){
+    public Animation<Sprite> createAnimation(String id, String atlasId, float frameDuration){
         Array<Sprite> sprites = atlasses.get(atlasId).createSprites(id);
 
         Animation<Sprite> animation = new Animation<Sprite>(frameDuration, sprites);
         animations.put(id, animation);
+
+        return animation;
+    }
+
+    /**
+     *  Loads an animation from the default atlas. The sprites are retrieved from the atlas and the animation will be stored with the alias as the key
+     * */
+    public Animation<Sprite> createAnimationAlias(String id, String alias, float frameDuration){
+        Array<Sprite> sprites = defaultAtlas.createSprites(id);
+
+        Animation<Sprite> animation = new Animation<Sprite>(frameDuration, sprites);
+        animations.put(alias, animation);
+
+        return animation;
+    }
+
+    /**
+     *  Loads an animation from the default atlas. The sprites are retrieved from the atlas and the animation will be stored with the alias as the key
+     * */
+    public Animation<Sprite> createAnimationAlias(String id, String alias, String atlasId, float frameDuration){
+        Array<Sprite> sprites = atlasses.get(atlasId).createSprites(id);
+
+        Animation<Sprite> animation = new Animation<Sprite>(frameDuration, sprites);
+        animations.put(alias, animation);
 
         return animation;
     }
@@ -198,12 +246,39 @@ public class SpriteAssetSystem extends PassiveSystem {
         return animations.get(animationName, null);
     }
 
+    public Sprite getAnimationFrame(String animationName, float time, boolean looping){
+        if( animations.containsKey(animationName) ){
+            Animation<Sprite> anim = animations.get(animationName);
+            return anim.getKeyFrame(time, looping);
+
+        }
+        return null;
+    }
+
     public Sprite getSprite(String spriteName) {
         return sprites.get(spriteName, null);
     }
 
     public void removeSprite(String spriteName){
         sprites.remove(spriteName);
+    }
+
+    /**
+     *
+     * @return The default atlas
+     */
+    public TextureAtlas getAtlas(){
+        return defaultAtlas;
+    }
+
+    /**
+     *
+     * @return The atlas identified by the supplied id. "default" will return the default atlas
+     */
+    public TextureAtlas getAtlas(String atlasId){
+        if (atlasId.equals("default"))
+            return defaultAtlas;
+        return atlasses.get(atlasId);
     }
 
     @Override
