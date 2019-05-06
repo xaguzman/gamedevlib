69e43e90839505b2856d4c73fad2096d79fa73f5
Xavier Guzman <xaguzman@gmail.com>
2019-05-06 00:39:19 -0500

    storing input in input detection system now

--- artemisodb/src/main/java/com/xaguzman/artemiscommons/systems/InputDetectionSystem.java
+++ artemisodb/src/main/java/com/xaguzman/artemiscommons/systems/InputDetectionSystem.java
@@ -1,9 +1,11 @@
 package com.xaguzman.artemiscommons.systems;
 
 import com.artemis.Aspect;
+import com.artemis.BaseEntitySystem;
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.Wire;
 import com.artemis.systems.IteratingSystem;
+import com.artemis.utils.IntBag;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.IntArray;
 import com.badlogic.gdx.utils.IntMap;
@@ -11,9 +13,11 @@ import com.xaguzman.artemiscommons.components.Input;
 import com.xaguzman.gdxcommons.input.InputConfiguration;
 
 /** Updates the entities input. Detects current and previous frame's input */
-public class InputDetectionSystem extends IteratingSystem {
+public class InputDetectionSystem extends BaseEntitySystem {
 
     @Wire ComponentMapper<Input> inputMapper;
+
+    int previousActions, currentActions;
 
     InputConfiguration inputConfiguration;
 
@@ -30,14 +34,45 @@ public class InputDetectionSystem extends IteratingSystem {
         inputConfiguration = cfg;
     }
 
-    @Override
-    protected void process(int entityId) {
-        Input input = inputMapper.get(entityId);
-        input.previousActions = input.currentActions;
-        input.currentActions = update(input);
+    public boolean isActionActive(int action){
+        return (currentActions & action) != 0;
     }
 
-    public int update(Input input) {
+    public boolean isAnyActionActive(int... actions){
+        for (int action : actions){
+            if ( (currentActions & action) != 0)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean justDidAction(int action){
+        return isActionActive(action) &&  ((previousActions & action) == 0);
+    }
+
+    public void addAction(int action){
+        currentActions |= action;
+    }
+
+    @Override
+    protected void processSystem() {
+        previousActions = currentActions;
+        currentActions = update();
+
+        IntBag actives = subscription.getEntities();
+        int[] ids = actives.getData();
+        for (int i = 0, s = actives.size(); s > i; i++) {
+            process(ids[i]);
+        }
+    }
+
+    protected void process(int entityId) {
+        Input input = inputMapper.get(entityId);
+        input.previousActions = previousActions;
+        input.currentActions = currentActions;
+    }
+
+    private int update() {
         int actions = 0;
 
         IntMap.Keys actionsIterator = inputConfiguration.keyboardMapping.keys();
